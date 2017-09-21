d894629bc7348105827ee4247a99b0e741e31514
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-09-21 14:17:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter Fahrenheit : ");
+        double fahre = 104;
+        double celci = ((5.0/9)*(fahre-32));
+
+
+
+        System.out.println(fahre+" grader Fahrenheit er lig : " + celci + " grader Celcius.");
+
+    }
+}
