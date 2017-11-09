a9a52880eab27db998dfabfed29cbd2c0fb414f3
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-09 09:52:52 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Enter Fahrenheit : ");
+        //Scanner der henter "userværdi"
         Scanner userIn = new Scanner(System.in);
+        //giver værdien "userværdi" til fahrenheit
         double fahre = userIn.nextDouble();
+        //Calc der udrenger celcius
         double celci = ((5.0/9)*(fahre-32));
 
 
-
+        //En sout, der printer hvad der er sket...
         System.out.println(fahre+" grader Fahrenheit er lig : " + celci + " grader Celcius.");
 
     }
