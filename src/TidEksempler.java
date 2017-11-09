e35ba76eb9cab6cad6c2c07b7d050c09a220a5e6
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-09 09:55:12 +0100

    Update TidEksempler.java

--- src/TidEksempler.java
+++ src/TidEksempler.java
@@ -1,5 +1,6 @@
 public class TidEksempler {
     public static void main(String[] args) {
+        //Printer sekunder der er gået siden 1.jan. 1970.
         long timeSek = System.currentTimeMillis()/1000;
         System.out.println(timeSek);
     }
